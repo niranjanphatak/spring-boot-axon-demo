1873f5f558366932630074d3f802173683a779b9
Niranjan Phatak <niru.phatak@gmail.com>
2018-01-15 22:39:52 +0530

    Initial Commit

---
+++ src/main/java/com/niranjan/axon/springbootaxondemo/SpringBootAxonDemoApplication.java
@@ -0,0 +1,12 @@
+package com.niranjan.axon.springbootaxondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAxonDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAxonDemoApplication.class, args);
+	}
+}
